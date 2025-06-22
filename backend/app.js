f0148cfa9ff3f1ba55820c8d7bd58d1235b12e53
Saisha <saishakarki751@gmail.com>
2025-06-22 23:05:12 +0545

    Add profile controller and routes; update middleware

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const app = express();
 
 // Import your auth routes
 const authRoutes = require('./routes/authRoutes');
+const profileRoutes = require('./routes/profileRoutes');
 
 // Middleware
 app.use(cors());
@@ -12,5 +13,6 @@ app.use(express.json());
 
 // Use auth routes under /api/auth
 app.use('/api/auth', authRoutes);
+app.use('/api/profile', profileRoutes);
 
 module.exports = app;
