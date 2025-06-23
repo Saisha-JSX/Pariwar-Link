6acb5a646147707862f2599abdf7cfd4016c1285
Saisha <saishakarki751@gmail.com>
2025-06-23 23:25:11 +0545

    update auth controller

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,5 +5,6 @@ const authController = require('../controllers/authController');
 router.post('/send-otp', authController.sendOtp);
 router.post('/verify-otp', authController.verifyOtpAndRegister);
 router.post('/login', authController.login);
+router.post('/logout', authController.logout); 
 
 module.exports = router;
